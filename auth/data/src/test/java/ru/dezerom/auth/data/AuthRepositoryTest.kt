e790f69b2a3dc98507d410ac3410c6f8dc3585e2
DeZerom <demid2001s@gmail.com>
2025-03-05 20:44:02 +0300

    Сделал тесты для репозитория

---
+++ auth/data/src/test/java/ru/dezerom/auth/data/AuthRepositoryTest.kt
@@ -0,0 +1,28 @@
+package ru.dezerom.auth.data
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import ru.dezerom.auth.data.network.AuthMockApi
+import ru.dezerom.auth.data.repositories.AuthRepositoryImpl
+import ru.dezerom.auth.data.sources.AuthNetworkSource
+import ru.dezerom.core.tools.errors.NetworkError
+
+internal class AuthRepositoryTest {
+    private val repo = AuthRepositoryImpl(AuthNetworkSource(AuthMockApi()))
+
+    @Test
+    fun testValidCredentials() = runBlocking {
+        val result = repo.authorize("qwe", "qwe")
+        assertTrue(result.isSuccess)
+        assertTrue(result.getOrNull() == true)
+    }
+
+    @Test
+    fun testInvalidCredentials() = runBlocking {
+        val result = repo.authorize("", "")
+        assertFalse(result.isSuccess)
+        assertTrue(result.exceptionOrNull() is NetworkError)
+    }
+}
