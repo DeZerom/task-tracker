14918549fec5b88a35242b4a19e6c6f93797f269
DeZerom <demid2001s@gmail.com>
2025-04-12 15:29:29 +0300

    Поправил тесты

--- tasks/data/src/main/java/ru/dezerom/tasks/data/repositories/TasksRepositoryMock.kt
+++ tasks/data/src/main/java/ru/dezerom/tasks/data/repositories/TasksRepositoryMock.kt
@@ -1,12 +1,13 @@
 package ru.dezerom.tasks.data.repositories
 
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import ru.dezerom.tasks.data.models.TaskDataModel
 import ru.dezerom.tasks.data.network.TasksMockApi
 import ru.dezerom.tasks.data.sources.TasksNetworkSource
 
 class TasksRepositoryMock(
-    dispatcher: CoroutineDispatcher,
+    dispatcher: CoroutineDispatcher = Dispatchers.Default,
 ): TasksRepository {
     private val impl = TasksRepositoryImpl(TasksNetworkSource(TasksMockApi(), dispatcher))
 
