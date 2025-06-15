2a70a4f28d880dce1b09291dcdcb735a634f65f1
DeZerom <demid2001s@gmail.com>
2025-06-15 14:57:40 +0300

    Пофиксил падения при неудачном рефреше токена

--- core/tools/src/main/java/ru/dezerom/core/tools/errors/CustomErrors.kt
+++ core/tools/src/main/java/ru/dezerom/core/tools/errors/CustomErrors.kt
@@ -2,8 +2,9 @@ package ru.dezerom.core.tools.errors
 
 import ru.dezerom.core.tools.R
 import ru.dezerom.core.tools.string_container.StringContainer
+import java.io.IOException
 
-class NetworkError(val messageRes: StringContainer): Exception()
+class NetworkError(val messageRes: StringContainer): IOException()
 
 fun unknownNetworkError() = NetworkError(StringContainer.StringRes(R.string.unknown_error))
 
